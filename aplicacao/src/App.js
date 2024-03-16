5dcc7b535c6460b82fe1dd49396923cc1e15a187
Guilhermee <tcluis6265@gmail.com>
2024-03-15 21:20:13 -0300

    alters in App.js

--- aplicacao/src/App.js
+++ aplicacao/src/App.js
@@ -1,26 +1,27 @@
 import { Component } from 'react';
 import './App.css';
 
-class App extends Component { // componente de classe
+class App extends Component{ // componente de classe
   state = {
-    name: 'Luisao',
-    counter: 0
-  };
+      name: 'Luisao',
+      counter: 0
+    };
 
+  
+  
+  handlePClick = () =>{
 
-  handlePClick = () => {
-
-    this.setState({ name: 'Guilherme' }, console.log(`nome mudado para Guilherme`));
+    this.setState({name:'Guilherme'}, console.log(`nome mudado para Guilherme`));
   }
 
   handleAClick = (event) => {
     //event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+    const {counter}  = this.state;
+    this.setState({counter: counter + 1});
 
   }
 
-  render() { // método
+  render(){ // método
 
     const { name, counter } = this.state;
     return (
@@ -28,10 +29,10 @@ class App extends Component { // componente de classe
         <header className="App-header">
           <p onClick={this.handlePClick}>
             {name} say Hello {":)  !"}
-            <br> </br>
+            <br></br>
             {counter}
           </p>
-          <a
+          <a  
             onClick={this.handleAClick}
             className="App-link"
             href="https://www.luisaodev.tech"
