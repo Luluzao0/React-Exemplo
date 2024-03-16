2e5ae58cb50cbceb79d1a5c34526cb446bc4a009
Guilhermee <tcluis6265@gmail.com>
2024-03-15 21:17:02 -0300

    initializing the react

---
+++ aplicacao/src/App.js
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component { // componente de classe
+  state = {
+    name: 'Luisao',
+    counter: 0
+  };
+
+
+  handlePClick = () => {
+
+    this.setState({ name: 'Guilherme' }, console.log(`nome mudado para Guilherme`));
+  }
+
+  handleAClick = (event) => {
+    //event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+
+  }
+
+  render() { // método
+
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p onClick={this.handlePClick}>
+            {name} say Hello {":)  !"}
+            <br> </br>
+            {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://www.luisaodev.tech"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
