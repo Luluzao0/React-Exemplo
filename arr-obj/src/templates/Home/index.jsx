49c63ce51813ae62f7ab2cbba02aa2fb7910570d
Guilhermee <tcluis6265@gmail.com>
2024-03-19 13:53:52 -0300

    working with texts inputs

--- arr-obj/src/templates/Home/index.jsx
+++ arr-obj/src/templates/Home/index.jsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import { loadPosts } from "../../utils/load-posts";
 import { Posts } from "../../components/Posts";
 import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 
 class Home extends Component {
   state = {
@@ -13,6 +14,7 @@ class Home extends Component {
     allPosts: [],
     page: 0,
     postPerPage: 5,
+    searchValue: "",
   };
 
   async componentDidMount() {
@@ -31,21 +33,53 @@ class Home extends Component {
   laodMorePosts = () => {
     const { page, postPerPage, allPosts, posts } = this.state;
     const nextPage = page + postPerPage;
-    const nextPost = allPosts.slice(nextPage, nextPage + postPerPage);
-    posts.push(...nextPost);
+    const nextPost = allPosts.slice(nextPage, nextPage + postPerPage); // fazendo pesquisa
+    posts.push(...nextPost); // enchendo
 
     this.setState({ posts, page: nextPage });
   };
 
+  handleChange = (e) => {
+    const { value } = e.target; // evento com target
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    const { posts, page, postPerPage, allPosts } = this.state;
+    const { posts, page, postPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postPerPage >= allPosts.length;
+    // !! converte para booleano
+
+    const filterPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts; // operação ternaria condição ? exp1 : exp2
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        <div className="search-container">
+          {!!searchValue && <h1>Search Value: {searchValue}</h1>}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
+
+        {filterPosts.length > 0 && <Posts posts={filterPosts} />}
+        {filterPosts.length === 0 && <p>Não existem Postcards</p>}
+
         <div className="button-container">
-          <Button text="Load more posts" onClick={this.laodMorePosts} disabled={noMorePosts} />
+          {!searchValue && (
+            /* 
+              button de busca baseado no valor inserido na busca
+            */
+            <Button
+              text="Load more posts"
+              onClick={this.laodMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
