5a68f200628ad9cb82b058d1637b4cf0590e14b2
Guilhermee <tcluis6265@gmail.com>
2024-03-19 01:07:54 -0300

    alters in props and organizing components

--- arr-obj/src/components/PostCard/index.jsx
+++ arr-obj/src/components/PostCard/index.jsx
@@ -1,7 +1,10 @@
+import './styles.css';
+
 export const PostCard = ({ id, title, body, cover })  => ( //props e componentes    
   <div className="post">
     <img src={cover} alt={title} />
     <div key={id} className="post-content">
+      <h1>{id}</h1>
       <h2>{title}</h2>
       <p> {body} </p>
     </div>
