80e97ec77a683c308e955aeef07502c6a199e7ad
Guilhermee <tcluis6265@gmail.com>
2024-03-20 00:53:34 -0300

    working with states again

--- arr-obj/src/index.js
+++ arr-obj/src/index.js
@@ -6,8 +6,10 @@ import Home from './templates/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Home numberToIncrement={2} /> 
   </React.StrictMode>
 );
+//setando o numberToIncrement
+
 
 
